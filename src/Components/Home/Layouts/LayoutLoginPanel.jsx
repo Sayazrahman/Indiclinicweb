ad66ddfac58ea6c50bab5bd8272ac8de108301d1
Syed Ayaz Rahman <syedayazrahman50@gmail>
2023-09-27 21:37:01 +0530

    Ayaz

---
+++ src/Components/Home/Layouts/LayoutLoginPanel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HomeNavbar from '../../HomeNavbar'
+import LoginPanel from '../../LoginPanel'
+import Footer from '../../Footer'
+
+export default function LayoutLoginPanel() {
+  return (
+    <>
+    <HomeNavbar/>
+    <LoginPanel/>
+    <Footer/>
+    
+    </>
+  )
+}
